441962b0f217552999b860c21b57d01f810d7329
Sahil Ratnani <sahil.ratnani@xoriant.com>
2023-04-09 19:55:51 +0530

    Update options of step1 form

--- bms/Modules/Inventory/View Controllers/CreateInventory/Views/StepperTableViewCells/CreateInventoryStepForm/Step1Form.swift
+++ bms/Modules/Inventory/View Controllers/CreateInventory/Views/StepperTableViewCells/CreateInventoryStepForm/Step1Form.swift
@@ -46,38 +46,57 @@ extension Step1Form: UICollectionViewDataSource {
             cell.collectionFormElement.setupDropdownField(id: fieldNo, fieldTitle: "BUID", options: options , placeHolder: "BUID")
 
         case 3:
-            let options = [DropDownModel(id: 1, name: "NIBM"),
-                           DropDownModel(id: 2, name: "Wakad")]
+            let options = [DropDownModel(id: 1, name: "Open"),
+                           DropDownModel(id: 2, name: "Pile"),
+                           DropDownModel(id: 3, name: "Well")]
             cell.collectionFormElement.setupDropdownField(id: fieldNo, fieldTitle: "Type of Foundation", options: options , placeHolder: "Type of Foundation")
 
         case 4:
-            let options = [DropDownModel(id: 1, name: "NIBM"),
-                           DropDownModel(id: 2, name: "Wakad")]
+            let options = [DropDownModel(id: 1, name: "Solid RCC Pier"),
+                           DropDownModel(id: 2, name: "Hollow Box"),
+                           DropDownModel(id: 3, name: "RCC Pier with Prestress Pier Cap")]
             cell.collectionFormElement.setupDropdownField(id: fieldNo, fieldTitle: "Type of Sub-Structure", options: options , placeHolder: "Type of Sub-Structure")
         
         case 5:
-            let options = [DropDownModel(id: 1, name: "NIBM"),
-                           DropDownModel(id: 2, name: "Wakad")]
+            let options = [DropDownModel(id: 1, name: "RCC Solid Slab"),
+                           DropDownModel(id: 2, name: "RCC T Girder"),
+                           DropDownModel(id: 3, name: "PSC T Girder"),
+                           DropDownModel(id: 4, name: "RCC box Girder"),
+                           DropDownModel(id: 5, name: "RCC twin cell box girder"),
+                           DropDownModel(id: 6, name: "PSC Box girder"),
+                           DropDownModel(id: 7, name: "PSC twin celll box girder"),
+                           DropDownModel(id: 8, name: "Steel Box Girder"),
+                           DropDownModel(id: 9, name: "Steel T Girder")]
             cell.collectionFormElement.setupDropdownField(id: fieldNo, fieldTitle: "Type of Super-Structure", options: options , placeHolder: "Type of Super-Structure")
 
         case 6:
-            let options = [DropDownModel(id: 1, name: "NIBM"),
-                           DropDownModel(id: 2, name: "Wakad")]
+            let options = [DropDownModel(id: 1, name: "Steel"),
+                           DropDownModel(id: 2, name: "Neoprene"),
+                           DropDownModel(id: 3, name: "POT PTFE"),
+                           DropDownModel(id: 4, name: "Spherical"),
+                           DropDownModel(id: 5, name: "Special")]
             cell.collectionFormElement.setupDropdownField(id: fieldNo, fieldTitle: "Type of Bearing", options: options , placeHolder: "Type of Bearing")
         
         case 7:
-            let options = [DropDownModel(id: 1, name: "NIBM"),
-                           DropDownModel(id: 2, name: "Wakad")]
+            let options = [DropDownModel(id: 1, name: "Burried Joint"),
+                           DropDownModel(id: 2, name: "Filler"),
+                           DropDownModel(id: 1, name: "Strip seal"),
+                           DropDownModel(id: 1, name: "modular"),
+                           DropDownModel(id: 1, name: "Special")]
             cell.collectionFormElement.setupDropdownField(id: fieldNo, fieldTitle: "Type of Expansion Joint", options: options , placeHolder: "Type of Expansion Joint")
         
         case 8:
-            let options = [DropDownModel(id: 1, name: "NIBM"),
-                           DropDownModel(id: 2, name: "Wakad")]
+            let options = [DropDownModel(id: 1, name: "RCC wearing"),
+                           DropDownModel(id: 2, name: "BC"),
+                           DropDownModel(id: 2, name: "BC with mastic")]
             cell.collectionFormElement.setupDropdownField(id: fieldNo, fieldTitle: "Type of Wearing Coars", options: options, placeHolder: "Type of Wearing Coars")
         
         case 9:
-            let options = [DropDownModel(id: 1, name: "NIBM"),
-                           DropDownModel(id: 2, name: "Wakad")]
+            let options = [DropDownModel(id: 1, name: "Steel"),
+                           DropDownModel(id: 2, name: "Hand rail - htype"),
+                           DropDownModel(id: 2, name: "RCC railing with 2 tier"),
+                           DropDownModel(id: 2, name: "RCC Railing with 3 tier"),
+                           DropDownModel(id: 2, name: "RCC Crash barrier")]
             cell.collectionFormElement.setupDropdownField(id: fieldNo, fieldTitle: "Type of Railing", options: options , placeHolder: "Type of Railing")
         
         case 10:
