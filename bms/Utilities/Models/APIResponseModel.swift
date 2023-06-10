2a24036f3308f92da203bc3f9ffd68210c6291a4
Sahil Ratnani <sahil.ratnani@xoriant.com>
2023-06-10 22:14:07 +0530

    Update status in APIResponseModel as computed property of statusString from API response

--- bms/Utilities/Models/APIResponseModel.swift
+++ bms/Utilities/Models/APIResponseModel.swift
@@ -7,18 +7,21 @@
 import ObjectMapper
 
 class APIResponseModel: RequestBody {
-    @objc dynamic var status: Int  = 0
+    var status: Int {
+        Int(statusString ?? "-1") ?? -1
+    }
+    @objc dynamic var statusString: String?
     @objc dynamic var message: String?
     @objc dynamic var response : String?
 
     enum ResponseKeys : String {
-        case status = "ResponseCode"
+        case statusString = "ResponseCode"
         case  message = "ResponseDesc"
         case response  = "ResponseData"
     }
      
      override func mapping(map: ObjectMapper.Map) {
-         self.status             <- map[ResponseKeys.status.rawValue]
+         self.statusString             <- map[ResponseKeys.statusString.rawValue]
          self.message            <- map[ResponseKeys.message.rawValue]
          self.response            <- map[ResponseKeys.response.rawValue]
      }
